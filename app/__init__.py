9fe3c174c4b62e493e2694756d2bc4867b224124
Rahul Kr. Sahu <kumkumgupta38786@gmail.com>
2025-06-17 20:43:35 +0530

    Added a beautiful UI

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ def create_app():
 
     db.init_app(app)
 
+    from app.routes.home import home_bp
+    app.register_blueprint(home_bp)
+
     from app.routes.customers import customer_bp
     app.register_blueprint(customer_bp, url_prefix="/customers")
 
